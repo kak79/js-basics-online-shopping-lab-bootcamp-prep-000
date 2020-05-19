11ba5ea5a63ed88ebb2c2c4ba82f4aad5750e6e0
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 15:49:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -95,8 +95,8 @@ function removeFromCart(item) {
       
    }
    
-    return "That item is not in your cart." ;
-    
+    console.log( "That item is not in your cart." );
+    return cart; 
 
 }
 
