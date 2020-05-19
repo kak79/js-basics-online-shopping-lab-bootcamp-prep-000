a0943f55e0895358d7a723639378e33275e5905d
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 17:52:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ function placeOrder(cardNumber) {
 
     else  {
       
-      return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
+      return `Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`;
       
     
     
