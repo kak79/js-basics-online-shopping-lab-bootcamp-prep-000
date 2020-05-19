873dbcbadf7746aee455dd2878b75022251b8698
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 15:11:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ function removeFromCart(item) {
       
       if  (cart[i].itemName === item ) {
         
-        cart.splice(i,1);
+        cart.slice(0,i)
         return item;
         
       }
