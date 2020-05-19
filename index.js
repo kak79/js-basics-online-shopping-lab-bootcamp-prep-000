65208e26e401a1cd9a5e48558d95022f8f436583
Kimberly Kohel-Hayes <kohelka@gmail.com>
2020-05-19 17:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -115,13 +115,15 @@ function placeOrder(cardNumber) {
     }
     
 
-    else if (cardNumber =! "" )){
+    else if (cardNumber = "" ){
       
       return `Your total cost is $${cartTotal}, which will be charged to the card ${cardNumber}.`;
       
     }
     
     else {
+      
+      
       
       
       
